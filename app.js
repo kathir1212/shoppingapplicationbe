9dd3e45b2f80929dd58b1711dd3566846e19cfbd
kathir1212 <kathirvel082@gmail.com>
2026-07-16 20:21:11 +0530

    url issues

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ await connectCloudinary()
 
 // const allowedOrigin = [
 //   "http://localhost:5173",        // for local dev
-const allowedOrigin = "https://shoppingapplicationfe.netlify.app/";
+const allowedOrigin = "https://shoppingapplicationfe.netlify.app";
 // const allowedOrigin = "http://localhost:5173";
 
 
