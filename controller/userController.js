d8ce39170c09101be7e5a15aa2465982dbf80bd4
kathir1212 <kathirvel082@gmail.com>
2026-07-16 20:04:50 +0530

    core issue fix

--- controller/userController.js
+++ controller/userController.js
@@ -24,11 +24,18 @@ export const register = async (req, res) => {
       expiresIn: '7d',
     });
 
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'None',
-    });
+    // res.cookie('token', token, {
+    //   httpOnly: true,
+    //   secure: true,
+    //   sameSite: 'None',
+    // });
+
+    res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
     return res.json({
       success: true,
